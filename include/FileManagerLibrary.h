c1bea1b895bd3a27cb259fc99c042fb4e8b1f01b
Tanner Ensign <tensign1444@gmail.com>
2023-04-20 09:53:55 -0700

    Initial commit

---
+++ include/FileManagerLibrary.h
@@ -0,0 +1,37 @@
+
+#ifndef FILESYSTEMMANAGER_FILEMANAGERLIBRARY_H
+#define FILESYSTEMMANAGER_FILEMANAGERLIBRARY_H
+
+#include <stdint.h>
+
+typedef struct _partition{
+    uint8_t bootable;
+    uint8_t first_chs[3];
+    uint8_t type;
+    uint8_t last_chs[3];
+    uint32_t lba_offset;
+    uint32_t sector_count;} __attribute__((packed)) partition;
+
+
+typedef struct _MBR{
+    uint8_t bootcode[446];
+    partition part[4];
+    uint16_t magic;} __attribute__((packed))MBR;
+
+
+
+typedef struct _DIR_ENTRY{
+    uint8_t name8[8];
+    uint8_t ext3[3];
+    uint8_t attributes;
+    uint16_t reserved;
+    uint16_t creation_time;
+    uint16_t creation_date;
+    uint16_t last_access_date;
+};
+
+typedef struct _ROOT_DIR{
+    struct _DIR_ENTRY
+};
+
+#endif //FILESYSTEMMANAGER_FILEMANAGERLIBRARY_H
---
+++ src/FileManagerLibrary.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "../include/FileManagerLibrary.h"
+
+static char g_working_directory[256] = {0};
+static FILE* g_file_pointer = NULL;
+static FAT* g_partition_list = NULL;
+static int g_partition_count = 0;
+
+const char* HumanNumberString(off_t size){
+    const off_t GIGABYTE = 1073741824;
+    const off_t MEGABYTE = 1048576;
+    const off_t KILOBYTE = 1024;
+
+    static char hsize[32] = {0};
+    memset(hsize,0,sizeof(hsize));
+}
+
+
+int ReadMBR(const char* filename) {
+   struct stat fs = {0};
+   int rc = stat(filename, &fs);
+
+   FILE* fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        printf("Error opening disk image file.\n");
+        return EXIT_FAILURE;
+    }
+   rc = fseek(fp,0, SEEK_SET);
+
+   char* buffer = (char*)malloc(sizeof(MBR));
+
+   memset(buffer,0,sizeof(MBR));
+   int count = fread(buffer,sizeof(char), sizeof(MBR), fp);
+
+   MBR *mbr = (MBR*)buffer;
+
+
+    for (int i = 0; i < 446; i++) {
+        printf("%02X ", mbr->bootcode[i]);
+    }
+}
+
