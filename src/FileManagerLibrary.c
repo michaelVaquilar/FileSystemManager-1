98cab6ec86cd67f2fa507480f444cd81a699d9b0
Masa-dotcom <116229167+Masa-dotcom@users.noreply.github.com>
2023-04-24 12:42:15 -0700

    Update FileManagerLibrary.c

--- src/FileManagerLibrary.c
+++ src/FileManagerLibrary.c
@@ -46,3 +46,47 @@ void dumpMBR(){
     }
 }
 
+
+void ListContents(const char* filename) {
+    // Open the image disk file
+    FILE* fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    // Calculate the location of the root directory
+    int rootDirLocation = 512 + (1 + (2 * 32)) * 512; // FAT Beginning + reserved sector + #FATs * sectors/FAT * bytes/sector
+    // Just check the location
+    printf("Root directory location: %d\n", rootDirLocation);
+    //Skip the boot sector FAT sector
+    fseek(fp, rootDirLocation, SEEK_SET);
+
+    // Read the root directory entries
+    char buffer[32]; //Each entry is 32 bytes
+    for (int i = 0; i < 224; i++) {
+        fread(buffer, sizeof(buffer), 1, fp);
+        // Check if the directory entry represents a file or subdirectory
+        if (buffer[0] == 0x00) {
+            continue;  // Unused or deleted entry
+        }
+        if (buffer[11] == 0x10) {
+            printf("<DIR> ");
+        }
+        else {
+            printf("<FILE> ");
+        }
+        
+        // Extract the file or directory name
+        char name[9];
+        strncpy(name, buffer, 8);
+        name[8] = '\0';
+        char extension[4];
+        strncpy(extension, buffer + 8, 3);
+        extension[3] = '\0';
+        printf("%s%s\n", name, extension);
+
+    }
+    // Close the disk image file
+    fclose(fp);
+}
